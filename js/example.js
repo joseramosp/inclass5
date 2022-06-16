076ac22582331d07095f307aff4676e3f1bd9499
Jose Ramos <jmrp.04@gmail.com>
2022-06-15 20:30:01 -0400

    Finishing the all the requirements

--- js/example.js
+++ js/example.js
@@ -1,10 +1,30 @@
+const page = document.getElementById('page');
+const list = document.getElementsByTagName('li');
+
 // ADD NEW ITEM TO END OF LIST
 
+var item1 = document.createElement("li");
+item1.appendChild(document.createTextNode("Chicken breast"));
+list[0].parentNode.appendChild(item1, list[0]);
 
 // ADD NEW ITEM START OF LIST
+
+var item2 = document.createElement("li");
+item2.appendChild(document.createTextNode("Cheese"));
+list[0].parentNode.insertBefore(item2, list[0]);
 
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
 
+for (var i = 0; i < list.length; i++) {
+    list[i].className = 'cool';
+}
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+
+var items_count = document.createElement("span");
+items_count.appendChild(document.createTextNode(list.length));
+
+const header = document.getElementsByTagName('h2');
+header[0].appendChild(items_count);
+// items_count.innerHTML()
